ffbc69b994cf2a97eab19e27f57d0a0b9e51d1d0
Dylane <tanolamene@gmail.com>
2025-03-01 10:14:42 -0400

    Fix bug API KEY

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_dWiQbwxR_5nst8fsZF98at3gawUmNTtMu");
 
 export async function POST(req: Request) {
   try {
